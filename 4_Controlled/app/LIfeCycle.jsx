12c62b890b94a284d4971065f1c5c25ed4572c37
sumitac007 <sumitchinchane@gmail.com>
2017-11-30 16:40:14 +0530

    test

---
+++ 4_Controlled/app/LIfeCycle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Root from './app/ControlledComponents.jsx';
+// import Root from './app/DefaultProps.jsx';
+// ReactDOM.render(<Root />, document.getElementById("container"));
+
+// import Root from './app/PropTypes.jsx';
+// ReactDOM.render(<Root now={new Date().toString()} age={40}/>, document.getElementById("container"));
+
+import Root from './app/LifeCycle.jsx';
+ReactDOM.render(<Root />, document.getElementById("container"));
